b10db991ce67de497080f805453a5422503dbee3
Viesturs <viesturs.mikus.rozite@gmail.com>
2021-07-19 23:13:09 +0300

    3rd task

---
+++ TheSolutionForArithmetics/SumAverageRunningInt/Program3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SumAverageRunningInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const decimal lowerBound = 1;
+            const decimal upperBound = 100;
+
+            decimal numberOfNumbers = upperBound - lowerBound + 1;
+            decimal midpointOfNums = (lowerBound + upperBound) / 2;
+
+            Console.WriteLine($"The sum of {lowerBound} to {upperBound} is {midpointOfNums * numberOfNumbers}\n" +
+                $"The average is {midpointOfNums}");
+
+
+
+        }
+    }
+}
