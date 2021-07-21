39ac4a74836b496fb4ed08adebc8d221fcf7750e
Viesturs <viesturs.mikus.rozite@gmail.com>
2021-07-21 09:41:25 +0300

    10th task

--- TheSolutionForArithmetics/CalculateArea/Geometry.cs
+++ TheSolutionForArithmetics/CalculateArea/Geometry.cs
@@ -8,19 +8,41 @@ namespace CalculateArea
 {
     public class Geometry
     {
-        public static double areaOfCircle(decimal radius)
+        
+        public static double? areaOfCircle(decimal radius)
         {
-            throw new NotImplementedException();
+            if(radius <= 0)
+            {
+                Console.WriteLine("Error, radius cannot be zero or below");
+                return null;
+            }
+            decimal area = (decimal)Math.PI * radius * 2;
+            return Convert.ToDouble(area);
+            //throw new NotImplementedException();
         }
 
-        public static double areaOfRectangle(decimal length, decimal width)
+        public static double? areaOfRectangle(decimal length, decimal width)
         {
-            throw new NotImplementedException();
+            if (length <= 0 || width <= 0)
+            {
+                Console.WriteLine("Error, nither length nor width can be zero or below");
+                return null;
+            }
+            decimal area = length * width;
+            return Convert.ToDouble(area);
+            //throw new NotImplementedException();
         }
 
-        public static double areaOfTriangle(decimal ground, decimal h)
+        public static double? areaOfTriangle(decimal ground, decimal h)
         {
-            throw new NotImplementedException();
+            if (ground <= 0 || h <= 0)
+            {
+                Console.WriteLine("Error, nither groud nor h can be zero or below");
+                return null;
+            }
+            decimal area = 0.5M * (ground * h);
+            return Convert.ToDouble(area);
+            //throw new NotImplementedException();
         }
     }
 }
