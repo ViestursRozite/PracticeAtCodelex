bd4bb1a0c624aab5c883d757f10cc1ec5690f986
Viesturs <viesturs.mikus.rozite@gmail.com>
2021-07-26 12:16:58 +0300

    5th task

---
+++ Loops/Exercise5/Program5.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program5
+    {
+        static void Main(string[] args)
+        {
+            int totalLineLengthInDots = 33;
+            string[] twoWords = new string[2];
+            string outputString = "";
+            string symbol = ".";
+
+            Console.WriteLine("Enter the 1st word: \n");
+            twoWords[0] = Console.ReadLine();
+
+            Console.WriteLine("Enter the 2nd word: \n");
+            twoWords[1] = Console.ReadLine();
+
+            foreach(string word in twoWords)
+            {
+                totalLineLengthInDots -= word.Length;
+            }
+
+            outputString += twoWords[0];
+            for (int i = 0; i < totalLineLengthInDots; i++)
+            {
+                outputString += $"{symbol}";
+            }
+            outputString += twoWords[1];
+
+            Console.WriteLine("\n" + outputString);
+            Console.WriteLine("length: " + outputString.Length);
+        }
+    }
+}
