76a4595a47fbafb1023781a0d4eb767fa9db641f
Viesturs <viesturs.mikus.rozite@gmail.com>
2021-07-27 11:36:40 +0300

    9th task

---
+++ Loops/RollTwoDice/Program9.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace RollTwoDice
+{
+    class Program9
+    {
+        static void Main()
+        {
+            int dice = 2;
+            int sides = 6;
+            int lowestOnDice = 1;
+            int minVal = dice * lowestOnDice;
+            int maxVal = dice * sides;
+            int userInput = 0;
+
+            Console.WriteLine($"I will roll {dice} d{sides}'s. \nEnter the sum you want to get:");
+            int.TryParse(Console.ReadLine(), out userInput);
+            if(userInput < minVal || userInput > maxVal)//get valid input
+            {
+                Console.WriteLine("... why?");
+                Main();
+            }
+
+            Console.WriteLine("Let's roll some dice!");
+            Random rnd = new Random();
+
+
+            int sumHolder = 0;
+            do
+            {
+                sumHolder = 0;
+                for (int i = 0; i < dice; i++)
+                {
+                    int roll = rnd.Next(lowestOnDice, sides + 1);
+                    Console.WriteLine($"Roll{i + 1}: {roll}");
+                    sumHolder += roll;
+                }
+                Console.WriteLine($"Summ: {sumHolder}\n");
+
+                if (sumHolder != userInput)
+                {
+                    Console.WriteLine($"Aww come on! One more try");
+                }
+
+            } while (sumHolder != userInput);
+
+            Console.WriteLine($"LETS GOOOOOOO! I got it! \n asked:{userInput} \nrolled:{sumHolder}");
+
+            Console.WriteLine("enter 'y' to play again, nothing to close");
+            string answer = Console.ReadLine().ToLower();
+            if(answer.Equals("y"))//end or play again?
+            {
+                Main();
+            }
+
+            Console.WriteLine("thanks for playing");
+            Console.ReadKey();
+        }
+    }
+}
