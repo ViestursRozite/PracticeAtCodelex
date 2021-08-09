c370d00f5a0533121ca1d1945abc9584ec8ebe16
Viesturs <viesturs.mikus.rozite@gmail.com>
2021-08-09 10:09:49 +0300

    7th task

---
+++ ClassesAndObjects/Exercise7/Dog.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise7
+{
+    class Dog
+    {
+        private string name, sex;
+        private Dog mother, father;
+
+        public Dog(string name, string sex)
+        {
+            this.name = name;
+            this.sex = sex;
+        }
+
+        public string Name
+        { get { return this.name; } }
+
+        public string Sex
+        { get { return this.sex; } }
+
+        public Dog Mother
+        { get { return this.mother; } set { this.mother = value; } }
+        public Dog Father
+        { get { return this.father; } set { this.father = value; } }
+
+        public string FathersName()
+        {
+            if (this.Father == null) return "Unknown";
+            return this.Father.Name;
+        }
+
+        public bool HasSameMotherAs(Dog checkThisDog)
+        {
+            return this.Mother == checkThisDog.Mother;
+        }
+
+
+    }
+}
---
+++ ClassesAndObjects/Exercise7/DogTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercise7
+{
+    class DogTest
+    {
+        static void Main(string[] args)
+        {
+            Dog Max, Rocky, Sparky, Buster, Sam, Lady, Molly, Coco;
+
+            Max = new Dog("Max", "male");
+            Rocky = new Dog("Rocky", "male");
+            Sparky = new Dog("Sparky", "male");
+            Buster = new Dog("Buster", "male");
+            Sam = new Dog("Sam", "male");
+            Lady = new Dog("Lady", "female");
+            Molly = new Dog("Molly", "female");
+            Coco = new Dog("Coco", "female");
+
+            Max.Mother = Lady;
+            Max.Father = Rocky;
+
+            Coco.Mother = Molly;
+            Coco.Father = Buster;
+
+            Rocky.Mother = Molly;
+            Rocky.Father = Sam;
+
+            Buster.Mother = Lady;
+            Buster.Father = Sparky;
+
+            Console.WriteLine(Coco.FathersName());
+            Console.WriteLine(Sparky.FathersName());
+
+            Console.WriteLine(Coco.HasSameMotherAs(Rocky));
+            Console.WriteLine(Coco.HasSameMotherAs(Buster));
+        }
+    }
+}
