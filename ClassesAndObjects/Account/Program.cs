c21288a44b54c55618344e0e8174453f4f81281b
Viesturs <viesturs.mikus.rozite@gmail.com>
2021-08-10 20:00:37 +0300

    8th task

--- ClassesAndObjects/Account/Program.cs
+++ ClassesAndObjects/Account/Program.cs
@@ -10,26 +10,122 @@ namespace Account
     {
         private static void Main(string[] args)
         {
-            var aAccount = new Account("A account", 100.00);
-            var bAccount = new Account("B account",0);
-            var cAccount = new Account("C Account", 0);
-            Console.WriteLine("Initial state");
-            Console.WriteLine(aAccount);
-            Console.WriteLine(bAccount);
-            Console.WriteLine(cAccount);
-            aAccount.Name = "GGGG";
-            Transfer(aAccount,bAccount, 50.0);
-            Transfer(bAccount,cAccount, 25.0);
-            
-            Console.WriteLine("Final state");
-            Console.WriteLine(aAccount);
-            Console.WriteLine(bAccount);
-            Console.WriteLine(cAccount);
+            //var aAccount = new SavingsAccount("A account", 100.00m);
+            //var bAccount = new SavingsAccount("B account",0);
+            //var cAccount = new SavingsAccount("C Account", 0);
+            //Console.WriteLine("Initial state");
+            //Console.WriteLine(aAccount.balance());
+            //Console.WriteLine(bAccount.balance());
+            //Console.WriteLine(cAccount.balance());
+            //aAccount.Name = "GGGG";
+            //Transfer(aAccount,bAccount, 50.0m);
+            //Transfer(bAccount,cAccount, 25.0m);
+
+            //Console.WriteLine("Final state");
+            //Console.WriteLine(aAccount);
+            //Console.WriteLine(bAccount);
+            //Console.WriteLine(cAccount);
+
+            //Console.WriteLine("Time passes");
+
+            //aAccount.CalcInterestMonth(1);
+
+            //bAccount.CalcInterestMonth(6);
+
+            //cAccount.CalcInterestMonth(12);
+            //Console.WriteLine("1 month");
+            //Console.WriteLine(aAccount);
+            //Console.WriteLine("6 months");            
+            //Console.WriteLine(bAccount);
+            //Console.WriteLine("1 year");            
+            //Console.WriteLine(cAccount);
+
+            var firstAccount = new SavingsAccount(AskForString("Give me the name of the first savings account"),
+                AskForDecimal("enter starting balance"), 
+                AskForDecimal("Enter a number representing yearly interest for this account(5% = 0.05)"));
+            var secondAccount = new SavingsAccount(AskForString("Give me the name of the second savings account"), 
+                AskForDecimal("enter starting balance"), 
+                AskForDecimal("Enter a number representing yearly interest for this account(5% = 0.05)"));
+
+            Console.WriteLine(firstAccount);
+            Console.WriteLine(secondAccount);
+
+            decimal monthsPassed = AskForDecimal("how many full months do we hold for?");
+
+            firstAccount.CalcInterestMonth(monthsPassed);
+            secondAccount.CalcInterestMonth(monthsPassed);
+
+            Console.WriteLine(firstAccount);
+            Console.WriteLine(secondAccount);
+
+            Console.WriteLine("Lets simulate a six month period of activity, press any key");
+            Console.ReadKey();
+
+            int deposits = 0;
+            int withdrawals = 0;
+
+            decimal deposited = 0;
+            decimal withdrawn = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                decimal deposit = AskForDecimal($"how much money do we deposit in the first account?\n month: {i + 1}/6");
+                deposited += deposit;
+                if (deposit != 0) deposits++;
+                firstAccount.deposit(deposit);
+                firstAccount.balance();
+
+                decimal withdraw = AskForDecimal($"how much do you take out?\n month: {i + 1}/6");
+                withdrawn += withdraw;
+                if (withdraw != 0) withdrawals++;
+                firstAccount.withdrawal(withdraw);
+                firstAccount.balance();
+
+                Console.WriteLine("Gain from interest:");
+                decimal holder = firstAccount.balance(); 
+                firstAccount.CalcInterestMonth(1);
+                Console.WriteLine(firstAccount.balance() - holder);
+
+                Console.WriteLine("Remainder:");
+                Console.WriteLine(firstAccount.balance());
+            }
+
+            Console.WriteLine("deposits: " + deposits);
+            Console.WriteLine("withdrawals: " + withdrawals);
+
+            Console.WriteLine("total deposited: " + deposited);
+            Console.WriteLine("total withdrawn: " + withdrawn);
 
             Console.ReadKey();
         }
 
-        public static void Transfer(Account from, Account to, double howMuch)
+        public static string AskForString(string sayThisToUser)
+        {
+            Console.WriteLine(sayThisToUser);
+            string input = "";
+            input = Console.ReadLine();
+            if (input.Equals(""))
+            {
+                Console.WriteLine("Try again");
+                AskForString(sayThisToUser);
+            }
+            return input;
+        }
+
+        public static decimal AskForDecimal(string sayThisToUser)
+        {
+            Console.WriteLine(sayThisToUser);
+            double input = -1;
+            double.TryParse(Console.ReadLine(), out input);
+            if ( input < 0)
+            {
+                Console.WriteLine("Try again");
+                AskForString(sayThisToUser);
+            }
+            return (decimal)input;
+        }
+
+        public static void Transfer(SavingsAccount from, SavingsAccount to, decimal howMuch)
         {
             to.deposit(from.withdrawal(howMuch));
         }
---
+++ ClassesAndObjects/Account/SavingsAccount.cs
@@ -0,0 +1,55 @@
+﻿namespace Account
+{
+    class SavingsAccount
+    {
+        
+        private decimal _anualInterest;
+        private string _name;
+        private decimal _balance;
+
+        public SavingsAccount(string name, decimal startingBalance, decimal anualInterest = 0.06m)
+        {
+            this._balance = startingBalance;
+            this._name = name;
+            this._anualInterest = anualInterest;
+        }
+
+        public decimal withdrawal(decimal i)
+        {
+            return this._balance += - i;
+        }
+
+        public void deposit(decimal i)
+        {
+            this._balance += i;
+        }
+
+        public decimal balance()
+        {
+            return _balance;
+        }
+
+        public override string ToString()
+        {
+            return $"{_name}: {_balance}";
+        }
+
+        public void CalcInterestMonth(decimal timeInMonths)
+        {
+            _balance *= (( (1 + (timeInMonths * _anualInterest / 12))));
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public decimal AnualInterest
+        {
+            get => _anualInterest;
+            set => _anualInterest = value;
+        }
+
+    }
+}
