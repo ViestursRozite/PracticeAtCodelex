6d96965c665899e5d8f3706730e9e6ee4b77d784
Viesturs <viesturs.mikus.rozite@gmail.com>
2021-08-11 22:06:46 +0300

    12th task

--- ClassesAndObjects/Account/Program.cs
+++ ClassesAndObjects/Account/Program.cs
@@ -10,64 +10,91 @@ namespace Account
     {
         private static void Main(string[] args)
         {
+            //Comented out to complete 12th task
+            //var firstAccount = new SavingsAccount(AskForString("Give me the name of the first savings account"),
+            //    AskForDecimal("enter starting balance"), 
+            //    AskForDecimal("Enter a number representing yearly interest for this account(5% = 0.05)"));
+            //var secondAccount = new SavingsAccount(AskForString("Give me the name of the second savings account"), 
+            //    AskForDecimal("enter starting balance"), 
+            //    AskForDecimal("Enter a number representing yearly interest for this account(5% = 0.05)"));
 
-            var firstAccount = new SavingsAccount(AskForString("Give me the name of the first savings account"),
-                AskForDecimal("enter starting balance"), 
-                AskForDecimal("Enter a number representing yearly interest for this account(5% = 0.05)"));
-            var secondAccount = new SavingsAccount(AskForString("Give me the name of the second savings account"), 
-                AskForDecimal("enter starting balance"), 
-                AskForDecimal("Enter a number representing yearly interest for this account(5% = 0.05)"));
+            //Console.WriteLine(firstAccount);
+            //Console.WriteLine(secondAccount);
 
-            Console.WriteLine(firstAccount);
-            Console.WriteLine(secondAccount);
+            //decimal monthsPassed = AskForDecimal("how many full months do we hold for?");
 
-            decimal monthsPassed = AskForDecimal("how many full months do we hold for?");
+            //firstAccount.CalcInterestMonth(monthsPassed);
+            //secondAccount.CalcInterestMonth(monthsPassed);
 
-            firstAccount.CalcInterestMonth(monthsPassed);
-            secondAccount.CalcInterestMonth(monthsPassed);
+            //Console.WriteLine(firstAccount);
+            //Console.WriteLine(secondAccount);
 
-            Console.WriteLine(firstAccount);
-            Console.WriteLine(secondAccount);
+            //Console.WriteLine("Lets simulate a six month period of activity, press any key");
+            //Console.ReadKey();
 
-            Console.WriteLine("Lets simulate a six month period of activity, press any key");
+            //int deposits = 0;
+            //int withdrawals = 0;
+
+            //decimal deposited = 0;
+            //decimal withdrawn = 0;
+
+            //for (int i = 0; i < 6; i++)
+            //{
+            //    decimal deposit = AskForDecimal($"how much money do we deposit in the first account?\n month: {i + 1}/6");
+            //    deposited += deposit;
+            //    if (deposit != 0) deposits++;
+            //    firstAccount.deposit(deposit);
+            //    firstAccount.balance();
+
+            //    decimal withdraw = AskForDecimal($"how much do you take out?\n month: {i + 1}/6");
+            //    withdrawn += withdraw;
+            //    if (withdraw != 0) withdrawals++;
+            //    firstAccount.withdrawal(withdraw);
+            //    firstAccount.balance();
+
+            //    Console.WriteLine("Gain from interest:");
+            //    decimal holder = firstAccount.balance(); 
+            //    firstAccount.CalcInterestMonth(1);
+            //    Console.WriteLine(firstAccount.balance() - holder);
+
+            //    Console.WriteLine("Remainder:");
+            //    Console.WriteLine(firstAccount.balance());
+            //}
+
+            //Console.WriteLine("deposits: " + deposits);
+            //Console.WriteLine("withdrawals: " + withdrawals);
+
+            //Console.WriteLine("total deposited: " + deposited);
+            //Console.WriteLine("total withdrawn: " + withdrawn);
+
+
+            /////////////////////////////////////////
+            //12th task
+
+            SavingsAccount MattsAcc = new SavingsAccount("Matt's account", 1000m, 0);
+            SavingsAccount MyAcc = new SavingsAccount("My account", 0m, 0);
+
+            MattsAcc.withdrawal(100);
+            MyAcc.deposit(100);
+
+            Console.WriteLine(MattsAcc.ToString());
+            Console.WriteLine(MyAcc.ToString());
+
+            //Testing the method
+            SavingsAccount A = new SavingsAccount("A", 100m, 0);
+            SavingsAccount B = new SavingsAccount("B", 0m, 0);
+            SavingsAccount C = new SavingsAccount("C", 0m, 0);
+
+            Transfer(A, B, (double)50);
+            Transfer(B, C, (double)25);
+
             Console.ReadKey();
+        }
 
-            int deposits = 0;
-            int withdrawals = 0;
-
-            decimal deposited = 0;
-            decimal withdrawn = 0;
-
-            for (int i = 0; i < 6; i++)
-            {
-                decimal deposit = AskForDecimal($"how much money do we deposit in the first account?\n month: {i + 1}/6");
-                deposited += deposit;
-                if (deposit != 0) deposits++;
-                firstAccount.deposit(deposit);
-                firstAccount.balance();
-
-                decimal withdraw = AskForDecimal($"how much do you take out?\n month: {i + 1}/6");
-                withdrawn += withdraw;
-                if (withdraw != 0) withdrawals++;
-                firstAccount.withdrawal(withdraw);
-                firstAccount.balance();
-
-                Console.WriteLine("Gain from interest:");
-                decimal holder = firstAccount.balance(); 
-                firstAccount.CalcInterestMonth(1);
-                Console.WriteLine(firstAccount.balance() - holder);
-
-                Console.WriteLine("Remainder:");
-                Console.WriteLine(firstAccount.balance());
-            }
-
-            Console.WriteLine("deposits: " + deposits);
-            Console.WriteLine("withdrawals: " + withdrawals);
-
-            Console.WriteLine("total deposited: " + deposited);
-            Console.WriteLine("total withdrawn: " + withdrawn);
-
-            Console.ReadKey();
+        public static void Transfer(SavingsAccount from, SavingsAccount to, double howMuch)//12th task method
+        {
+            from.withdrawal((decimal)howMuch);
+            to.deposit((decimal)howMuch);
         }
 
         public static string AskForString(string sayThisToUser)
