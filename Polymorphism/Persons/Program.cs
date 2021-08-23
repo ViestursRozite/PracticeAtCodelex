5c659df0207668a9cb1ab4931145e649469ff38f
Viesturs <viesturs.mikus.rozite@gmail.com>
2021-08-23 10:22:23 +0300

    3rd task

--- Polymorphism/Persons/Program.cs
+++ Polymorphism/Persons/Program.cs
@@ -6,10 +6,24 @@ namespace Persons
     {
         static void Main(string[] args)
         {
-            /*
-             * - Create an instant of class Student and an instant of class Employee with proper values for the attributes.
-             * - Display the content of each class using Display() method.
-             */
+            Student student1 = new Student(3);
+            Student student2 = new Student(4, "Adam", "Biggs", "Main Street 1", 2558522);
+
+            Employee employee1 = new Employee("Programmer");
+            Employee employee2 = new Employee("Coder", "Jon", "Smalls", "Baker street 2", 96335);
+
+            student1.Display();
+            Console.WriteLine();
+
+            student2.Display();
+            Console.WriteLine();
+
+            employee1.Display();
+            Console.WriteLine();
+
+            employee2.Display();
+
+            Console.ReadKey();
         }
     }
 }
