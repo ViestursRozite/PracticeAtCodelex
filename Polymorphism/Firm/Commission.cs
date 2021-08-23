29a8187a73acdcbf44bab85bd8c00ad4be7a8a29
Viesturs <viesturs.mikus.rozite@gmail.com>
2021-08-23 11:47:57 +0300

    4th task

---
+++ Polymorphism/Firm/Commission.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Firm
+{
+    public class Commission : Hourly
+    {
+        private double CommissionRate { get; set; }
+        private double TotalSales { get; set; }
+        public Commission(
+            string eName, 
+            string eAddress, 
+            string ePhone, 
+            string socSecNumber, 
+            double rate,
+
+            double percentOnSales)
+            
+            :base(eName, eAddress, ePhone, socSecNumber, rate)
+        {
+            this.CommissionRate = percentOnSales;
+        }
+
+        public void addSales(double totalSales)
+        {
+            this.TotalSales += totalSales;
+        }
+
+        public override double Pay()
+        {
+            var earned = base.Pay() + (this.TotalSales * this.CommissionRate);
+            TotalSales = 0;
+            //don't need to set the hoursWorked back to 0—why not?
+            //cause base.Pay() sets _hoursWorked = 0
+            return earned;
+        }
+
+        public override string ToString()
+        {
+            var result = base.ToString();
+            result += "\nTotals sales: " + this.TotalSales;
+            return result;
+        }
+    }
+}
