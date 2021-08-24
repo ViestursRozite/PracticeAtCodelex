7024cce799e6c3937919525d6d978fa0dc17041a
Viesturs <viesturs.mikus.rozite@gmail.com>
2021-08-24 15:50:25 +0300

    6th task

---
+++ Polymorphism/Hierarchy/Food.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hierarchy
+{
+    public abstract class Food
+    {
+        public int Quantity { get; set; }
+        public string FoodClass { get; set; }
+        public Food(int quantity)
+        {
+            this.Quantity = quantity;
+        }
+    }
+}
---
+++ Polymorphism/Hierarchy/Mouse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hierarchy
+{
+    public class Mouse : Mammal
+    {
+        private string Sound = "*ultrasonic noises";
+        public Mouse(string animalName, double animalWeight, int foodEaten, string livingRegion, string animalType = "Mouse"):
+            base(animalName, animalType, animalWeight, foodEaten, livingRegion)
+        { }
+
+        public override void Eat(Food food, int quantity)
+        {
+            if(food.FoodClass.Equals("Vegitable"))
+            {
+                base.Eat(food, quantity);
+            }
+            else
+            {
+                Console.WriteLine($"{base.AnimalType} do not eat that type of food!");
+            }
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine(this.Sound);
+        }
+    }
+}
